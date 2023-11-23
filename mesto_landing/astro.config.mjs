263e05a3b87e01a460f25a14d48958faca05ec81
neoko <33611354+neokofg@users.noreply.github.com>
2023-11-23 14:07:48 +0700

    fix/landing

--- mesto_landing/astro.config.mjs
+++ mesto_landing/astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind(), react()]
+  integrations: [svelte(), tailwind(), react()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
